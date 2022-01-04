34d7eb60a015c22f2d9148e309d86022024afcc9
Roman Prokop <roman.prokop@inoxoft.com>
2022-01-04 16:41:05 +0200

    stream examples

---
+++ 17-http-stream.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+
+http
+	.createServer((req, res) => {
+		// const text = fs.readFileSync('./content/big.txt', 'utf8')
+		// res.end(text)
+
+		//chunked
+		const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+		fileStream.on('open', () => {
+			fileStream.pipe(res)
+		})
+		fileStream.on('error', (err) => {
+			res.end(err)
+		})
+	})
+	.listen(5759)
